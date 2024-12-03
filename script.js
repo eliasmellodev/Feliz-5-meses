b45b14e71a6fbabd26d8de34d6b9f5f5be4d6fcc
805eliasmello <149224437+805eliasmello@users.noreply.github.com>
2024-12-03 01:01:37 -0300

    dados

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // script.js
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', () => {
-        console.log("Botão clicado: " + button.textContent);
-    });
-});
+let currentIndex = 0;
+const images = document.querySelectorAll(".carousel-images img");
+const totalImages = images.length;
+
+function showNextImage() {
+    const carousel = document.querySelector(".carousel-images");
+    currentIndex = (currentIndex + 1) % totalImages;
+    const offset = -currentIndex * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
+}
+
+setInterval(showNextImage, 3000);
